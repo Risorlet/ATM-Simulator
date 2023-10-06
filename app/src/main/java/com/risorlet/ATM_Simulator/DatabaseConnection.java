1455241f89faa4e34986a589df08e2421e4ea665
Ar_Lazy_Dev <arghaghosh2801@gmail.com>
2023-10-07 01:54:56 +0530

    Created DatabaseConnection.java
    Created the java class DatabaseConnection which will be used to connect the application with the database. Also updated the build.gradle file to add the JDBC driver for MySQL as a runtime dependency.

---
+++ app/src/main/java/com/risorlet/ATM_Simulator/DatabaseConnection.java
@@ -0,0 +1,30 @@
+package com.risorlet.ATM_Simulator;
+
+import java.sql.*;
+
+public class DatabaseConnection {
+    
+    String url = "jdbc:mysql://localhost/atm_simulator";
+    String userName = "root";
+    String password = "root@2801";
+
+    Connection conn;
+    Statement st;
+
+    DatabaseConnection(){
+
+        try {
+            // Loads the JDBC driver for MySQL
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            // Creates a connection between the driver and the database
+            conn = DriverManager.getConnection(url, userName, password);
+
+            // Creates a statement which will be used to execute queries
+            st = conn.createStatement();
+            
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
